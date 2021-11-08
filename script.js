bc4f07d68f05ff90c8f68e7718949a6a1e012578
Alex <example@gmail.com>
2021-11-08 21:12:28 +0200

    save comething

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?", "");
-      b = prompt("На сколько оцените его?", "");
-      c = prompt("Один из последних просмотренных фильмов?", "");
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
       d = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies.a = b;
-personalMovieDB.movies.c = d;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
